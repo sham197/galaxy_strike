31c4ef5242e675cd6262e1dba5426a579d5f6e6a
sham197 <samilzp@gmail.com>
2025-12-25 14:11:11 +0200

    Add laser particle system and weapon script
    Introduced new particle sprites and materials for laser effects, added a Laser GameObject with particle system to the SampleScene, and implemented PlayerWeapon.cs to handle weapon functionality. Updated PlayerMovement.cs and scene assets to integrate the new laser system.

--- Galaxy_Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy_Strike/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,47 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float controlSpeed = 10f;
+    [SerializeField] private float xClampRange = 30f;
+    [SerializeField] private float yClampRange = 30f;
+    [SerializeField] private float controlRollFactor = 20f;
+    [SerializeField] private float controlPitchFactor = 20f;
+    [SerializeField] private float rotationSpeed = 10f;
+    
+    private Vector2 _movement;
 
-    // Update is called once per frame
     void Update()
     {
+        UpdatePlayerPosition();
+        ProcessRotation();
+    }
+
+    private void UpdatePlayerPosition()
+    {
+        var yOffset = _movement.y * (controlSpeed * Time.deltaTime);
+        var xOffset = _movement.x * (controlSpeed * Time.deltaTime);
         
+        var rawXPos = transform.localPosition.x + xOffset;
+        var rawYPos = transform.localPosition.y + yOffset;
+        
+        var clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+        var clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+    
+    private void ProcessRotation()
+    {
+        var pitch = -controlPitchFactor * _movement.y * rotationSpeed;
+        var roll = -controlRollFactor * _movement.x * rotationSpeed;
+        
+        var targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime);
     }
     
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        _movement = value.Get<Vector2>();
     }
 }
