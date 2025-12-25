a09665a76e93ad21d27dd57860f4d37d1ec08cdd
sham197 <samilzp@gmail.com>
2025-12-25 17:56:40 +0200

    Add laser prefab and crosshair, enable firing
    Added a new Laser prefab and associated material for player weapon effects. Introduced a crosshair image asset. Updated PlayerControls to include a Fire action mapped to the spacebar. Modified PlayerMovement and PlayerWeapon scripts, and updated the SampleScene to support firing functionality.

--- Galaxy_Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy_Strike/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float controlRollFactor = 20f;
     [SerializeField] private float controlPitchFactor = 20f;
     [SerializeField] private float rotationSpeed = 10f;
-    
+
     private Vector2 _movement;
 
     void Update()
@@ -22,24 +22,24 @@ public class PlayerMovement : MonoBehaviour
     {
         var yOffset = _movement.y * (controlSpeed * Time.deltaTime);
         var xOffset = _movement.x * (controlSpeed * Time.deltaTime);
-        
+
         var rawXPos = transform.localPosition.x + xOffset;
         var rawYPos = transform.localPosition.y + yOffset;
-        
+
         var clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
         var clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
-    
+
     private void ProcessRotation()
     {
-        var pitch = -controlPitchFactor * _movement.y * rotationSpeed;
-        var roll = -controlRollFactor * _movement.x * rotationSpeed;
-        
+        var pitch = -controlPitchFactor * _movement.y;
+        var roll = -controlRollFactor * _movement.x;
+
         var targetRotation = Quaternion.Euler(pitch, 0f, roll);
         transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime);
     }
-    
+
     public void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
--- Galaxy_Strike/Assets/Scripts/PlayerWeapon.cs
+++ Galaxy_Strike/Assets/Scripts/PlayerWeapon.cs
@@ -1,6 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject[] lasers;
+    [SerializeField] private RectTransform crosshair;
+    [SerializeField] private Transform targetPoint;
+    [SerializeField] private float targetDistance = 100;
+    private bool _isFiring = false;
+    private ParticleSystem.EmissionModule _emissionModule;
+    private Vector2 _mousePosition;
+
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        ProcessFiring();
+        MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
+    }
+
+    private void OnFire(InputValue value)
+    {
+        _isFiring = value.isPressed;
+    }
+
+    private void ProcessFiring()
+    {
+        foreach (var laser in lasers)
+        {
+            _emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            _emissionModule.enabled = _isFiring;
+        }
+    }
+
+    private void MoveCrosshair()
+    {
+        _mousePosition = Mouse.current.position.ReadValue();
+        crosshair.position = _mousePosition;
+    }
+
+    private void MoveTargetPoint()
+    {
+        var targetPointPosition = new Vector3(_mousePosition.x, _mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    private void AimLasers()
+    {
+        foreach (var laser in lasers)
+        {
+            var fireDirection = targetPoint.position - this.transform.position;
+            var rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
+    }
 }
