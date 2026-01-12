ce7e8cdb9ad056837a247d51873e06fa06e872e3
sham197 <samilzp@gmail.com>
2026-01-12 15:32:47 +0200

    Add scoring system and enemy hit points
    настроил scoreboard для префабов противников

--- Galaxy_Strike/Assets/Scripts/Enemy.cs
+++ Galaxy_Strike/Assets/Scripts/Enemy.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
-    
+    [SerializeField] int hitPoints = 6;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard _scoreboard;
+
+    private void Start()
+    {
+        _scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log("HIT");
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        hitPoints -= 1;
+        if (hitPoints <= 0)
+        {
+            Debug.Log("HIT");
+            _scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Galaxy_Strike/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    int score = 0;
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+    }
+}
